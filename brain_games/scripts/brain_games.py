62e45007bb10f832aaf488e46a988df80b8b8c51
Константин <volkovkostik988@gmail.com>
2022-05-25 20:04:46 +0400

    add new function and modul prompt

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python
+import prompt
 def main():
     print('Welcome to the Brain Games!')
+    name = prompt.string('May I have your name? ')
+    print(f'Hello, {name}!')
 
 
 if __name__ == "__main__":

0cd2b81a1ed4de68f60ef367c98fb2b4c301710f
Константин <volkovkostik988@gmail.com>
2022-06-01 18:02:11 +0400

    minor fixed

--- brain_games/games/gcd_game.py
+++ brain_games/games/gcd_game.py
@@ -6,7 +6,7 @@ REGULATIONS = "Find the greatest common divisor of given numbers."
 def get_start():
     first_number = randrange(1, 40)
     second_number = randrange(1, 40)
-    question = f'{first_number} {second_number}'
+    question = f'{first_number}' f'{second_number}'
     while first_number != 0 and second_number != 0:
         if first_number >= second_number:
             first_number %= second_number

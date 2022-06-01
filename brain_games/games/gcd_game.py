01e50add607dbd35ec93a0d576ff5f6a5fe93a17
Константин <volkovkostik988@gmail.com>
2022-06-01 17:33:06 +0400

    minor fixed

--- brain_games/games/gcd_game.py
+++ brain_games/games/gcd_game.py
@@ -9,8 +9,8 @@ def get_start():
     question = (f'{first_number} {second_number}')
     while first_number != 0 and second_number != 0:
         if first_number >= second_number:
-            first_number %= second_number
+            first_number = first_number % second_number
         else:
-            second_number %= first_number
+            second_number = second_number % first_number
     correct_answer = first_number or second_number
     return question, str(correct_answer)
